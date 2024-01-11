5de8ec73d6a87c6a44ac9116d28bde48266159e9
Eduardo Roque <roquee82@gmail.com>
2024-01-11 11:46:30 -0600

    Add routing

--- src/index.js
+++ src/index.js
@@ -2,14 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import Fetch from './components/AllUsers';
+import PostsUser from './components/PostsUser';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+library.add( fab, faUser );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+          <Route path="/" exact element={<App/>} />
+          <Route path="/users" exact element={<Fetch/>} />
+          <Route path="/users/:id" element={<PostsUser />} />
+      </Routes>
+    </Router>
+
   </React.StrictMode>
+  
+
+  
 );
+
+
+  
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
