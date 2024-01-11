80425f41a643a2938a2ce165daf322ab3bf2165b
Eduardo Roque <roquee82@gmail.com>
2024-01-11 14:42:32 -0600

    Modified the map of  users for resolve error when moving back in the browser and clicking on another user.

--- src/components/AllUsers.js
+++ src/components/AllUsers.js
@@ -10,16 +10,16 @@ const Fetch = () => {
           return res.json();
         })
         .then((data) => {
-          console.log(data);
+          //console.log(data);
           setUsers(data);
         });
     }, []);
     return (
       <div className='blog-post-page'>
-        <div className='main-content bg'>
+        <div className='main-content-users bg'>
           
-          {users.map((user) => (
-            <Link to={`/users/${user.id}`} className='item-user'>
+          {users !== null && users.map((user) => (
+            <Link to={`/users/${user.id}`} className='item-user' key={user.id}>
               <span className='icon-user'><FontAwesomeIcon icon="fa-solid fa-user" /></span>
               <p>{user.name}</p>
               <p>{user.email}</p>
