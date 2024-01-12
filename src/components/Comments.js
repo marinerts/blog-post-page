45dae2f6203bfe6470214acb3f2e487344410a79
Eduardo Roque <roquee82@gmail.com>
2024-01-11 20:09:29 -0600

    Change structure comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Comments = ({ postId }) => {
   const [comments, setComments] = useState([]);
@@ -22,11 +23,15 @@ const Comments = ({ postId }) => {
  
 
   return (
-    <ul>
+    <ul className='list-comments-post'>
       {comments !== null && comments.map((comment) => (
-        <li key={comment.id}>
-          <strong>{comment.name}</strong>
-          <p>{comment.body}</p>
+        <li className='item-comment' key={comment.id}>
+            <div className='container-user-comment'>
+                <span className='icon-user'><FontAwesomeIcon icon='fa-solid fa-user' /></span>
+                <p>{comment.email}</p>
+            </div>  
+            <strong className='name-comment'>{comment.name}</strong>
+            <p className='body-comment'>{comment.body}</p>
         </li>
       ))}
     </ul>
